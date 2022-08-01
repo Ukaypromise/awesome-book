cef12de77556208868d465775899c4512001015b
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-08-01 19:58:32 +0100

    Add fuctions to add & remove and save to local storage

--- script/app.js
+++ script/app.js
@@ -1,28 +1,55 @@
 // Get Items
-const title = document.getElementById("title")
-const author = document.getElementById("author")
-const addBtn = document.querySelector(".btn-add")
-const body = document.querySelector("body")
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const addBtn = document.querySelector(".btn-add");
+const body = document.querySelector("body");
 
-// Array of objects container books
-let books = [{
-  title: 'The Cake Baker',
-  author: 'Miss Evelyn',
-}, {
-  title: 'Javascript Mastery',
-  author: 'John Doe',
-}];
+// Array of objects containing books
+let books = [
+  {
+    title: "The Cake Baker",
+    author: "Miss Evelyn",
+  },
+  {
+    title: "Javascript Mastery",
+    author: "John Doe",
+  },
+];
 
 // Book Constructor function (representing a book).
-function Book(){
-    this.title =title;
-    this.author =author;
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+}
+// functions to Handle tasks
+// Add Books
+function addBook(title, author) {
+  const newBook1 = new Book(title, author);
+  books.push(newBook1);
+}
+// Remove Books
+function removeBook(title, author) {
+  books = books.filter((book) => {
+    return book.title !== title && book.author !== author;
+  });
+}
+// Store function (To handle storage)
+const  bookAwesome = {
+  title:'',
+  author:'',
+  bookList:[],
+}
+// Saving To storage
+function saveToStorage(bookAwesome){
+  bookAwesome.bookList = books;
+  bookAwesome.title = titleInput.value;
+  bookAwesome.author = authorInput.value;
+  const storeBooks = JSON.stringify(bookAwesome);
+  localStorage.setItem('bookAwesome',storeBooks);
+
 }
 
-
-// UI constructor (To handle UI Tasks)
-// Store constructor function (To handle storage)
 // Events =>> Display Books
 // Events =>> Add a Books
 // Events =>> Remove a Books
-// 
+//
