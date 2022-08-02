d4a4acf05232e5847b33c2c9686b891095d36345
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-08-02 13:34:55 +0100

    Add horizontal line below remove button and fix linter errors

--- script/app.js
+++ script/app.js
@@ -1,52 +1,56 @@
 /* eslint-disable max-classes-per-file */
-const inputTitle = document.getElementById("title");
-const inputAuthor = document.getElementById("author");
-const submitBtn = document.querySelector(".add-btn");
-const bookSection = document.querySelector(".books");
+const inputTitle = document.getElementById('title');
+const inputAuthor = document.getElementById('author');
+const submitBtn = document.querySelector('.add-btn');
+const bookSection = document.querySelector('.books');
 class BookSet {
   // Constructor with array of books
   constructor(books = []) {
     this.books = books;
   }
+
   // Events =>> Add a Books
   add(bookItem) {
     this.books.push(bookItem);
     this.display(bookItem);
     this.remove();
     this.saveToStorage();
-    inputAuthor.value = "";
-    inputTitle.value = "";
+    inputAuthor.value = '';
+    inputTitle.value = '';
   }
 
   remove() {
-    const removeBtns = document.querySelectorAll(".remove-button");
-    removeBtns[removeBtns.length - 1].addEventListener("click", (e) => {
+    const removeBtns = document.querySelectorAll('.remove-button');
+    removeBtns[removeBtns.length - 1].addEventListener('click', (e) => {
       this.removeFromColl(e.target);
       bookSection.removeChild(e.target.parentNode);
     });
   }
+
   // Events =>> Display Books Dynamically
   display(data) {
     if (this) {
-      const div = document.createElement("div");
-      div.className = "book-wraper";
+      const div = document.createElement('div');
+      div.className = 'book-wraper';
       div.innerHTML = `<h4>"${data.title}" by
                     ${data.author}</h4>
-                    <button data-value="${data.title}-${data.author}" type="button" class ="remove-button">Remove</button>`;
+                    <button data-value="${data.title}-${data.author}" type="button" class ="remove-button">Remove</button><hr />`;
       bookSection.appendChild(div);
     }
   }
+
   // Remove Books
   removeFromColl(data) {
-    const arr = data.getAttribute("data-value").split("-");
+    const arr = data.getAttribute('data-value').split('-');
     this.books = this.books.filter(
-      (item) => item.title + item.author !== arr[0] + arr[1]
+      (item) => item.title + item.author !== arr[0] + arr[1],
     );
     this.saveToStorage();
   }
+
   // Saving To storage
   saveToStorage() {
-    localStorage.setItem("bookItems", JSON.stringify({ bookColl: this.books }));
+    localStorage.setItem('bookItems', JSON.stringify({ bookColl: this.books }));
   }
 }
 
@@ -59,13 +63,13 @@ class Book {
 }
 
 const coll = new BookSet();
-if (localStorage.getItem("bookItems")) {
-  const localBooks = JSON.parse(localStorage.getItem("bookItems"));
+if (localStorage.getItem('bookItems')) {
+  const localBooks = JSON.parse(localStorage.getItem('bookItems'));
   localBooks.bookColl.forEach((item) => {
     coll.add(new Book(item.title, item.author));
   });
 }
 
-submitBtn.addEventListener("click", () => {
+submitBtn.addEventListener('click', () => {
   coll.add(new Book(inputTitle.value, inputAuthor.value));
 });
