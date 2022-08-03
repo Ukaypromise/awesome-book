07039e4499e5c6eb7c72b478d29bd3f7ccc49c13
asnganje <abd.nganje@gmail.com>
2022-08-03 22:50:40 +0300

    Modify index.html, style.css and app.js

--- script/app.js
+++ script/app.js
@@ -74,3 +74,36 @@ if (localStorage.getItem('bookItems')) {
 submitBtn.addEventListener('click', () => {
   coll.add(new Book(inputTitle.value, inputAuthor.value));
 });
+
+const dateVisit = document.querySelector('.userDate');
+dateVisit.innerHTML = new Date();
+
+const listLink = document.querySelector('.books');
+const booksAddition = document.querySelector('.bookAdded');
+const contactUs = document.querySelector('.contactUs');
+const navigator = document.querySelectorAll('.book-nav');
+
+navigator.forEach((n, index) => n.addEventListener('click', () => {
+    navigator.forEach((link, number) => {
+        if (number === index) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
+
+    if(index === 0) {
+        listLink.classList.remove('hidden');
+        booksAddition.classList.add('hidden');
+        contactUs.classList.add('hidden');
+    } else if(index == 1){
+        listLink.classList.add('hidden');
+        booksAddition.classList.remove('hidden');
+        contactUs.classList.add('hidden');
+    } else {
+        listLink.classList.add('hidden');
+        booksAddition.classList.add('hidden');
+        contactUs.classList.remove('hidden');
+    }
+})); 
+
